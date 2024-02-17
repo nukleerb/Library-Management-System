8d7ec7f4d35e336cf197711c074bc16a828b1bdc
baris <147567811+nukleerb@users.noreply.github.com>
2024-02-17 13:15:05 +0300

    Update library manage.py

--- library manage.py
+++ library manage.py
@@ -2,7 +2,7 @@ class Library :
 
     def __init__(self, listofbooks):
 
-          self.availablebooks = listofbooks
+          self.avaiblebooks = listofbooks
 
 seek('Book_list.txt')
 
@@ -14,26 +14,39 @@ print ('Book_list.txt')
     print(f.(all(Book_list.txt)))
 
     close('Book_list.txt')
+print(""" ======LIBRARY MENU=======
+                1. List of all available books
+                2. Add a book
+                3. Remove a book
+                """)
+choice = int(input("Enter Choice:"))
+if choice == 1:
+    library.listofallavaiblebooks()
+elif choice == 2:
+    library.addabook(student.addBook())
+elif choice == 3:
+    library.removeabook(student.removeBook())
+
 
 
 def displayAvailablebooks(self):
     print("The books we have in our library are as follows:")
 
     print("================================")
-    for book in self.availablebooks:
+    for book in self.avaiblelebooks:
      print(book)
 
 
 def lendbook(self, requestedBook):
-    if requestedBook in self.availablebooks:
+    if requestedBook in self.avaiblebooks:
         print("The book you requested has now been borrowed")
-        self.availablebooks.remove(requestedBook)
+        self.avaiblebooks.remove(requestedBook)
     else:
         print("Sorry the book you have requested is currently not in the library")
 
 
 def addbook(self, returnedBook):
-    self.availablebooks.append(returnedBook)
+    self.avaiblebooks.append(returnedBook)
     print("Thanks for returning your borrowed book")
 
 
